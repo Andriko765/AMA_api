ef1c94b986931f8e6d0aa10c96ee2f17aee599cf
Andriko765 <andrik096108@gmail.com>
2024-08-09 17:46:41 +0300

    MongoModdifire error fixed

--- products.route.js
+++ products.route.js
@@ -1,36 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('./db');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const db = require("./db");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 // Apply CORS middleware
 
 router.use(cors());
 
 // Mock data for demonstration purposes
-router.get('/products', async (req, res) => {
-	try {
-		const collections = await db.db.listCollections().toArray();
-		const products = [];
+router.get("/products", async (req, res) => {
+  try {
+    const collections = await db.db.listCollections().toArray();
+    const products = [];
 
-		for (const collection of collections) {
-			const collectionName = collection.name;
-			const model = mongoose.model(
-				collectionName,
-				new mongoose.Schema({}, { strict: true })
-			);
-			const collectionProducts = await model.find().exec();
+    for (const collection of collections) {
+      const collectionName = collection.name;
+      let model = mongoose.models[collectionName];
 
-			if (collectionProducts.length > 0) {
-				products.push({ collectionName, products: collectionProducts });
-			}
-		}
-		res.json(products);
-	} catch (err) {
-		console.error(err);
-		res.status(500).json({ error: 'Error fetching products' });
-	}
+      if (!model) {
+        model = mongoose.model(
+          collectionName,
+          new mongoose.Schema({}, { strict: false })
+        );
+      }
+      const collectionProducts = await model.find().exec();
+
+      if (collectionProducts.length > 0) {
+        products.push({ collectionName, products: collectionProducts });
+      }
+    }
+    res.json(products);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error fetching products" });
+  }
 });
 
 module.exports = router;
